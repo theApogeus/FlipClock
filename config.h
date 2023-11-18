74d9355b83c1240e2f3fc1e7d96536ef90dbfa59
theApogeus <133257479+theApogeus@users.noreply.github.com>
2023-11-18 20:23:26 -0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define WIFI_SSID                    "YOUR_SSID"
+#define WIFI_PASSWORD                "YOUR_PASS"
+
+#define NTP_SERVER                   "pool.ntp.org"
+
+#define GMT_OFFSET_SEC               -10800 //= UTC−03:00 : -3*60*60=-10800
+#define DAY_LIGHT_OFFSET_SEC         0
+
+#define EXAMPLE_LCD_H_RES            320
+#define EXAMPLE_LCD_V_RES            240
